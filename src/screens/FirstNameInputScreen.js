5aaa7a6b55afa4e29421eb881c2a2d5e710334f8
misiyok <isiyokm1993@gmail.com>
2020-03-29 20:18:41 +0300

    first commit at dev branch: added a js file

---
+++ src/screens/FirstNameInputScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const FirstNameInputScreen = () => {
+    return <Text style={{ fontSize: 48 }}>FirstNameInputScreen</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default FirstNameInputScreen;
